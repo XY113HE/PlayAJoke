7cd1c929e3c0315f5ab327a4fdfec970378b7362
XY113HE <1148237542@qq.com>
2020-05-13 09:01:11 +0800

    1,大数值乘小数算法完成

--- app/src/main/java/com/gamess/playajoke/MainActivity.java
+++ app/src/main/java/com/gamess/playajoke/MainActivity.java
@@ -3,11 +3,12 @@ package com.gamess.playajoke;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 
 import com.gamess.playajoke.utils.BigDataTools;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +18,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void test() {
-        new BigDataTools().setData("4423758534451353843253144343215474331353843253144");
+        new BigDataTools().setData("132321283918642891794247109270391723491027401973143222315332546548523784982341451521214311").mult(20).printResult();
     }
 }
--- app/src/main/java/com/gamess/playajoke/utils/BigDataTools.java
+++ app/src/main/java/com/gamess/playajoke/utils/BigDataTools.java
@@ -10,9 +10,10 @@ import android.util.Log;
  * 输出规定 要求输出结果保留2位小数，如果有零则舍去
  */
 
+//将大数每3位存于一个数组内
 public class BigDataTools {
     private int[] bigbuff;
-    public void setData(String value){//4423758534451353843253144343215474331353843253144
+    public BigDataTools setData(String value){//4423758534451353843253144343215474331353843253144
         bigbuff = new int[100];
         for (int i = 0; i < bigbuff.length; i++){
             bigbuff[i] = -1;
@@ -26,28 +27,59 @@ public class BigDataTools {
                 bigbuff[99 - i] = Integer.parseInt(value.substring(value.length() - (i + 1) * 3, value.length() - i * 3));
             }
         }
-
-//        StringBuilder testsb = new StringBuilder();
-//        for (int i = 0; i < bigbuff.length; i++){
-//            if(bigbuff[i] != -1) {
-//                testsb.append(bigbuff[i]);
-//            }
-//        }
-//        Log.e("bigdata", "setData: " + testsb.toString());
+        return this;
     }
 
-    public String mult(int value){//2~9
-        StringBuilder sb = new StringBuilder();
+    //大数进行乘法  先进行分组的相乘并记录，再进行依次满100进位
+    public BigDataTools mult(int value){//2~9
         for(int i = 99; bigbuff[i] != -1; i--){
-            int temp = bigbuff[i]*value;
-            bigbuff[i] = temp%1000;
-            addData();
+            bigbuff[i] *= value;
         }
-        return sb.toString();
+        for(int i = 99; bigbuff[i] != -1; i--){
+            addData(i, bigbuff[i]);
+        }
+        return this;
     }
 
 
-    public void addData(){
+    //循环进位
+    private void addData(int i, int temp){
+        if(temp > 999){
+            bigbuff[i] = temp%1000;
+            if(bigbuff[i-1] == -1) {
+                temp = temp/1000;
+            }else {
+                temp = bigbuff[i - 1] + temp / 1000;
+            }
+            addData(i-1, temp);
+        }else{
+            bigbuff[i] = temp;
+        }
+    }
 
+    //输出计算后的结果
+    public void printResult(){
+        StringBuilder sb = new StringBuilder();
+        boolean firstIndex = true;
+        for (int i = 0; i < bigbuff.length; i++){
+            if(bigbuff[i] != -1) {
+                //除第一位不需要添加0，后面位置需要补0
+                if(firstIndex) {
+                    sb.append(bigbuff[i]);
+                    firstIndex = false;
+                }else{
+                    if(bigbuff[i] < 10){
+                        sb.append("00").append(bigbuff[i]);
+                    }else if(bigbuff[i] < 100){
+                        sb.append("0").append(bigbuff[i]);
+                    }else if(bigbuff[i] == 0){
+                        sb.append("000");
+                    }else{
+                        sb.append(bigbuff[i]);
+                    }
+                }
+            }
+        }
+        Log.e("bigdata", "result: " + sb.toString());
     }
 }
